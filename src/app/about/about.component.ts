25bea2d2a5cb4aea81a6f9ea243f107b5e139b83
hendersonanalytics <everett@hendersonanalytics.com>
2020-09-17 12:47:15 -0500

    end imperative design lesson

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,16 +15,6 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const http$ = createHttpObservable(APP_API.GET_COURSES);
 
-    const courses$ = http$.pipe(
-      map((response) => response.payload)
-    );
-
-    courses$.subscribe(
-      (courses) => console.log(courses),
-      noop,
-      () => console.log('completed')
-    );
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
+import {interval, noop, Observable, of, timer} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
 
+import {createHttpObservable} from '../common/util'
+import { APP_API } from 'app/constants/api';
 
 @Component({
     selector: 'home',
@@ -11,14 +13,27 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+  public beginnerCourses: Course[] = [];
+  public advancedCourses: Course[] = [];
 
     constructor() {
 
     }
 
     ngOnInit() {
+      const http$ = createHttpObservable(APP_API.GET_COURSES);
 
+      const courses$ = http$.pipe(
+        map((response) => response.payload)
+      );
 
+      courses$.subscribe(
+        (courses: Course[]) => {
+          this.beginnerCourses = courses.filter(c => c.category === 'BEGINNER');
+          this.advancedCourses = courses.filter(c => c.category === 'ADVANCED');        },
+        noop,
+        () => console.log('completed')
+      );
 
     }
 
